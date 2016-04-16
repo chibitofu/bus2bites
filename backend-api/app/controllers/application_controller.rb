02b2b504b30a61438f991300efeed6b44ace8dd7
Loraine <loraine@lorainekv.com>
2016-04-16 14:50:11 -0700

    Updated for cors allowance.

--- backend-api/app/controllers/application_controller.rb
+++ backend-api/app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
+  protect_from_forgery except: :index 
+  before_filter :add_cors_headers
+
+  def add_cors_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
 end
