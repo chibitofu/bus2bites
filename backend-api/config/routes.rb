fdcaec10a18f88eb432d4e9d41132889089d5d6e
Loraine <loraine@lorainekv.com>
2016-04-16 13:42:31 -0700

    Get route and controller.

--- backend-api/config/routes.rb
+++ backend-api/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/routes/:id' => 'restaurants#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
