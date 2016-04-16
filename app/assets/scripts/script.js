894a1410c33254d3f77fb5394d11818cd3178f32
chibitofu <erinmoony@gmail.com>
2016-04-16 15:58:52 -0700

    Searching now takes you to a list of results.

--- app/assets/scripts/script.js
+++ app/assets/scripts/script.js
@@ -1,21 +1,38 @@
-$(document).ready(function() {
-
-
-});
-
 $('#sub-butt').on('click', function(e) {
   e.preventDefault();
   var busRoute = parseInt($('.bus-route').val());
+  var places;
 
   $.ajax({
     url: 'http://localhost:3000/routes/:id',
     method: 'GET',
     data: busRoute,
     success: function(data, status) {
-      console.log(status);
+      places = data;
+      $('#route-form').hide()
+      routeList(places);
     },
     error: function(xhrt, status, error) {
       console.log(status);
     }
   })
+
 });
+
+
+function routeList(places) {
+  var items = places.restaurants
+  for(var i = 0; i < items.length; i++) {
+    $('#results').append(
+      '<li class="restaurants">' +
+      '<h3>' +
+      items[i].name +
+      '</h3>' +
+      '<p>' +
+      items[i].image +
+      '</p>' +
+      '</li>'
+    );
+  }
+
+}
